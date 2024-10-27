e3b748168b6d8868a8147a1b8ca3b3dbfc323385
al <66003967+301262177@users.noreply.github.com>
2024-10-27 16:27:17 +0800

    Update diy-part1.sh

--- RAX3000M-EMMC/diy-part1.sh
+++ RAX3000M-EMMC/diy-part1.sh
@@ -15,6 +15,9 @@
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #git clone https://github.com/messense/aliyundrive-webdav package/messense
 git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
+git clone https://github.com/noiver/luci-app-jd-dailybonus package/luci-app-jd-dailybonus
+git clone https://github.com/linkease/istore-ui package/istore-ui
+git clone https://github.com/linkease/istore package/istore
