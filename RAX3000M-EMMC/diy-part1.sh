c387680df5ab1aeac0f9287640af93389ec7a760
al <66003967+301262177@users.noreply.github.com>
2024-10-28 20:36:08 +0800

    Update diy-part1.sh

--- RAX3000M-EMMC/diy-part1.sh
+++ RAX3000M-EMMC/diy-part1.sh
@@ -14,27 +14,24 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 # ttyd免登陆
 sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
-# Git稀疏克隆，只克隆指定目录到本地
-function git_clone() {
-  branch="$1" repourl="$2" && shift 2
-  git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
-  repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
-  cd $repodir && git -checkout set $@
-  mv -f $@ ../package
-  cd .. && rm -rf $repodir
-}
+
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #git clone https://github.com/messense/aliyundrive-webdav package/messense
+
 # 应用过滤
 git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
+
 # 京东签到
 git clone https://github.com/noiver/luci-app-jd-dailybonus package/luci-app-jd-dailybonus
+
 # 商店
 git clone https://github.com/linkease/istore-ui package/istore-ui
 git clone https://github.com/linkease/istore package/istore
+
 # 网易云解灰（天灵）
 #git clone https://github.com/UnblockNeteaseMusic/server package/server
+
 # 简单mesh
 git clone https://github.com/ntlf9t/luci-app-easymesh package/luci-app-easymesh
