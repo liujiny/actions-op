faba6210c76fe21c9d7eb02afc55a161672569f3
al <66003967+301262177@users.noreply.github.com>
2024-10-30 15:04:36 +0800

    Update diy-part1.sh

--- RAX3000M-EMMC/diy-part1.sh
+++ RAX3000M-EMMC/diy-part1.sh
@@ -13,7 +13,7 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 # ttyd免登陆
-sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
+#sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
@@ -24,7 +24,7 @@ sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.
 git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
 
 # 京东签到
-git clone https://github.com/noiver/luci-app-jd-dailybonus package/luci-app-jd-dailybonus
+#git clone https://github.com/noiver/luci-app-jd-dailybonus package/luci-app-jd-dailybonus
 
 # 商店
 git clone https://github.com/linkease/istore-ui package/istore-ui
@@ -34,4 +34,4 @@ git clone https://github.com/linkease/istore package/istore
 #git clone https://github.com/UnblockNeteaseMusic/server package/server
 
 # 简单mesh
-git clone https://github.com/ntlf9t/luci-app-easymesh package/luci-app-easymesh
+#git clone https://github.com/ntlf9t/luci-app-easymesh package/luci-app-easymesh
