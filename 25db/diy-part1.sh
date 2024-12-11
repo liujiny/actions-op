94173ec1f27422e3353464f72b167797388fb0bb
al <66003967+301262177@users.noreply.github.com>
2024-12-11 23:02:54 +0800

    Update diy-part1.sh

--- 25db/diy-part1.sh
+++ 25db/diy-part1.sh
@@ -17,4 +17,3 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #git clone https://github.com/messense/aliyundrive-webdav package/messense
-#git clone https://github.com/destan19/OpenAppFilter package/OpenAppFilter
